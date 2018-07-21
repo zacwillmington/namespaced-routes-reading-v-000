efd343acd963fd867d56f6a1812e0bcc61df6f32
Zac Willmington <zac.willmington@gmail.com>
2018-07-21 01:49:11 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
-  get '/stats', to: 'stats/index'
+  get 'admin/stats', to: 'stats/index'
   
   root 'posts#index'
 end
